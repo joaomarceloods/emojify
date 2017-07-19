a26bc6e1b7eaf8ad96d44c88bc84751241aa0d0f
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-07-18 22:30:27 -0300

    Convert text to emoji text using a simple hash

--- App.js
+++ App.js
@@ -1,23 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { TouchableWithoutFeedback, Keyboard, StyleSheet, Text, View, TextInput } from 'react-native'
+import emojis from 'emojis-hash'
+
+const replaceEmoji = (text) => (
+  text
+    .split(' ')
+    .map(word => emojis[word.toLowerCase()] || word)
+    .join(' ')
+)
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { text: '', emojiText: '...and emojify it!' }
+    this.onChangeText = this.onChangeText.bind(this)
+  }
+
+  onChangeText(text) {
+    this.setState({
+      text,
+      emojiText: replaceEmoji(text)
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{flex: 1}}>
+          <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+            <Text style={{fontSize: 30, padding: 20, textAlign: 'center'}}>
+              Emojify
+            </Text>
+          </View>
+          <View style={{flex: 1, backgroundColor: 'skyblue'}}>
+            <TextInput
+              multiline={true}
+              value={this.state.text}
+              placeholder='Type something here...'
+              placeholderTextColor='black'
+              onChangeText={this.onChangeText}
+              style={{fontSize: 20, flex: 1, textAlignVertical: 'center', textAlign: 'center', padding: 10}}
+            />
+          </View>
+          <View style={{flex: 3, backgroundColor: 'steelblue'}}>
+            <Text style={{fontSize: 20, flex: 1, textAlignVertical: 'center', textAlign: 'center', color: 'white', padding: 10}}>
+              {this.state.emojiText}
+            </Text>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    )
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
